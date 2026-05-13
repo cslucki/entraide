c4b02f152515cbd09a2ed3b4d44cdcee24a4ef00
Cyril <cyril@teletravailleurs.com>
2026-05-13 22:16:11 +0200

    refactor(referral): move reward values to config

---
+++ config/referral.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'rewards' => [
+        'activation' => [
+            'level_1_referrer' => 20,
+            'level_2_referrer' => 10,
+        ],
+
+        'invitation' => [
+            'level_1_referrer' => 10,
+            'welcome' => 10,
+            'level_2_referrer' => 5,
+        ],
+    ],
+];
