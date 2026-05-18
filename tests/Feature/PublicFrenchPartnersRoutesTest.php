9a09a2a9d2206554c7ae1a70b1126a589ea5e439
Cyril <cyril@teletravailleurs.com>
2026-05-18 10:38:20 +0200

    feat(partners): add public French partner routes

---
+++ tests/Feature/PublicFrenchPartnersRoutesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Community;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PublicFrenchPartnersRoutesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_partenaires_index_is_public(): void
+    {
+        $this->get('/partenaires')
+            ->assertOk()
+            ->assertSee('Devenir partenaire');
+    }
+
+    public function test_partenaires_demande_is_public(): void
+    {
+        $this->get('/partenaires/demande')
+            ->assertOk()
+            ->assertSee('Devenir partenaire');
+    }
+
+    public function test_boucles_creer_redirects_to_partenaires_demande(): void
+    {
+        Community::factory()->create(['is_active' => true]);
+
+        $this->get('/boucles/creer')
+            ->assertRedirect('/partenaires/demande');
+    }
+
+    public function test_boucles_index_does_not_redirect_to_partenaires(): void
+    {
+        Community::factory()->create(['is_active' => true]);
+
+        $this->get('/boucles')
+            ->assertOk()
+            ->assertSee('Les Boucles');
+    }
+}
