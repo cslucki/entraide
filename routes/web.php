8a14c4a9147cc735a433e99873b67dfe4c3a9687
Cyril <cyril@teletravailleurs.com>
2026-05-25 20:57:26 +0200

    fix(route): remove duplicate route names blocking route:cache (T140.5I)

--- routes/web.php
+++ routes/web.php
@@ -383,9 +383,9 @@ Route::prefix('/org/{organization}')
 
         Route::middleware('guest')->group(function () {
             Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-            Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+            Route::post('/login', [AuthenticatedSessionController::class, 'store']);
             Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-            Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+            Route::post('/register', [RegisteredUserController::class, 'store']);
             Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
             Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
             Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
