5e49ac74e2491d200d27b01f752e8746295b9ba8
Cyril <cyril@teletravailleurs.com>
2026-06-24 00:27:39 +0200

    fix: persist org middleware for livewire updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Events\LoopMessageCreated;
+use App\Http\Middleware\ResolveOrganization;
 use App\Jobs\GenerateAiAgentResponse;
 use App\Models\AiConfig;
 use App\Models\BugReport;
@@ -53,6 +54,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -142,6 +144,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+
+        Livewire::addPersistentMiddleware(ResolveOrganization::class);
 
         try {
             $dbProvider = AiConfig::get('default_provider');
